f964e42785ef08bcf003a8f52321d35f83069bab
Adam Jackson <adam@xdmakesmusic.com>
2024-03-24 14:27:52 +0800

    Added template DSP component as a comment

--- XDDSP.cpp
+++ XDDSP.cpp
@@ -7,6 +7,65 @@
 
 #include "XDDSP.h"
 
+
+/*
+ Plugin DSP Component Template
+ 
+ Here is a neat little template for a component to encapsulate an entire DSP network
+ 
+ class PluginDSP : public Component<PluginDSP>
+ {
+  // Private data members here
+ public:
+  static constexpr int Count = 2;
+  
+  PluginInput<Count> signalIn;
+  Output<Count> signalOut;
+  
+  PluginDSP(Parameters &p) :
+  signalOut(p)
+  {}
+  
+  // This function is responsible for clearing the output buffers to a default state when
+  // the component is disabled.
+  void reset()
+  {
+   signalOut.reset();
+  }
+  
+  // startProcess prepares the component for processing one block and returns the step
+  // size. By default, it returns the entire sampleCount as one big step.
+ // int startProcess(int startPoint, int sampleCount)
+ // { return std::min(sampleCount, StepSize); }
+
+  // stepProcess is called repeatedly with the start point incremented by step size
+  void stepProcess(int startPoint, int sampleCount)
+  {
+   for (int c = 0; c < Count; ++c)
+   {
+    for (int i = startPoint, s = sampleCount; s--; ++i)
+    {
+     // DSP work done here
+    }
+   }
+  }
+  
+  // finishProcess is called after the block has been processed
+ // void finishProcess()
+ // {}
+ };
+ */
+
+
+
+
+
+
+
+
+
+
+
 namespace XDDSP {
 
 
